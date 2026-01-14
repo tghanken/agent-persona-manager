a08b216cde1d0f9a2be3919687c8d329d3107bfa
Taylor Hanken <tghanken@gmail.com>
2026-01-14 13:41:48 -0800

    feat: add garnix ci

--- nix/packages/default.nix
+++ nix/packages/default.nix
@@ -1,14 +1,2 @@
-{
-  pkgs,
-  inputs,
-  ...
-}: let
-  inherit (inputs.self.lib.${pkgs.stdenv.hostPlatform.system}) craneLib commonArgs;
-
-  cargoArtifacts = inputs.self.packages.${pkgs.stdenv.hostPlatform.system}.workspace-deps;
-  pname = "persona";
-in
-  craneLib.buildPackage (commonArgs
-    // {
-      inherit cargoArtifacts pname;
-    })
+{perSystem, ...}:
+perSystem.self.persona-cli
